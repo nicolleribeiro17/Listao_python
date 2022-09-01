50a4369e68e7df5925f4591a18a75e0f2f1c382a
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-01 16:51:09 -0300

    Exercicio 1

---
+++ ex1.py
@@ -0,0 +1,33 @@
+# 1) O Python trabalha tipos de valores. Com os valores abaixo, dê o nome de seus tipos:
+
+# a. 1 - tipo inteiro
+a = 1
+print(type(a))
+
+# b. 12.6 - tipo float
+b = 12.6
+print(type(b))
+
+# c. True - tipo bool
+c = True
+print(type(c))
+
+# d. False
+d = False
+print(type(d))
+
+# e. -543
+e = -543
+print(type(e))
+
+# f. -5.78
+f = -5.78
+print(type(f))
+
+# g. “copo”
+g = 'copo'
+print(type(g))
+
+# h. ‘Belo dia
+h = 'Belo dia'
+type(h)
