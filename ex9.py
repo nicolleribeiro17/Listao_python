886a2dab4d8433964d1ba567bde2821bcd34eff1
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 13:37:46 -0300

    docs: resolução exercício 9

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# 9) Qual é o resultado de cada linha de comando do Python? Siga a ordem dos comandos.
+
+valor = input("Informe um valor: ")
+print("Valor informado: ", valor)
+tipo = type(valor)
+
+x_str = "123"
+x = int(x_str)
+xf = float(x_str)
+
+sao_iguais = x == xf
+
+print("Um float é igual a um int?", sao_iguais)
