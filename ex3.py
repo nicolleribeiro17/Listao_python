0deca895518a687094ebd8b0c73b15887b9000e2
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 00:11:04 -0300

    docs: exercicio 3

---
+++ ex3.py
@@ -0,0 +1,49 @@
+# 3) Determine qual é o resultado dos seguintes cálculos no Python:
+
+# a. Operadores matemáticos
+
+# i - Soma
+i = 10 + 3
+print(f'Valor de i: {i}')
+
+# ii - Subtração
+ii = 10 - 3
+print(f'Valor de ii: {ii}')
+
+# iii - Multiplicação
+iii = 10 * 3
+print(f'Valor de iii: {iii}')
+
+# iv - Divisão
+iv = 10 / 3
+print(f'Valor iv: {iv}')
+
+# v - Divisão
+v = 10 / 3.0
+print(f'Valor de v: {v}')
+
+# vii - Divisão
+vii = 13 / 3.0
+print(f'Valor de vii: {vii}')
+
+# viii - Divisão inteira
+viii = 13 // 3.0
+print(f'Valor de viii: {viii}')
+
+# b. Ordem dos operadores
+
+# i - Multiplica e depois soma
+i = 5 + 30 * 20
+print(f'Valor de i: {i}')
+
+# ii - Faz a soma separado e após multiplica o resultado por 20
+ii = ((5 + 30) * 20)
+print(f'Valor de ii: {ii}')
+
+# iii - Faz a soma depois a multplicação e após todas as operações divide
+iii =  ((5 + 30) * 20) / 10
+print(f'Valor de iii: {iii}')
+
+# iv - Faz a multiplicação, ai com esse resultado faz a divisão por 10 e após essas operações soma o valor 5
+iv = 5 + 30 * 20 / 10
+print(f'Valor de iv: {iv}')
