d2c5c17fdd7964b32aa811e7dc24f56a58ffc65e
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 13:41:01 -0300

    docs: resolução exercício 11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+# 11) Programa Python no arquivo ex11.py: 
+# Este programa irá calcular a área de um quadrado. Peça para a pessoa informar a medida numérica de um 
+# lado do quadrado.  E depois informe-lhe o valor da área do quadrado.
+
+lado = int(input('Digite a medida numérica de um lado do quadrado: '))
+
+area = lado * lado
+print(f'A área do quadrado vale: {area}')
