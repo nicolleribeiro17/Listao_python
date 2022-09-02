9b98e05d995b011e714c0c79e2b6f93062e4290d
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 13:42:22 -0300

    docs: resolução exercício 13

---
+++ ex13.py
@@ -0,0 +1,9 @@
+# 13). Crie o seguinte programa Python no arquivo ex13.py:
+# Colete a idade de 3 pessoas e mostre a média de suas idades.
+
+idade1 = int(input('Digite a primeira idade: '))
+idade2 = int(input('Digite a segunda idade: '))
+idade3 = int(input('Digite a terceira idade: '))
+
+media = (idade_1 + idade_2 + idade_3) / 3
+print(f'A média das idades é de: {media}')
