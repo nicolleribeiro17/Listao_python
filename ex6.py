c7da91829579fa4fbea2d0f9e8d74163349ce321
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 13:21:58 -0300

    docs: resolução exercício 6

---
+++ ex6.py
@@ -0,0 +1,13 @@
+# 6) Qual será o valor de z? Qual seria outra forma de escrever este trecho de código?
+
+z = 3
+z += 2
+z *= 6
+z /= 5
+
+print(f'Valor de z: {z}')
+
+#outra maneira
+
+z = ((3 + 2) * 6) / 5
+print(f'Valor de z: {z}')
