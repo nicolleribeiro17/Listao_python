cd73a5f15a7443ee5b296006571aea7d0b68fef5
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-02 13:42:34 -0300

    docs: resolução exercício 14

---
+++ ex14.py
@@ -0,0 +1,9 @@
+# 14) Crie o seguinte programa Python no arquivo ex14.py:
+# Colete a idade de duas pessoas. E informe se a primeira idade é maior do que a da primeira. 
+# Neste aqui, basta responder True para informar que a primeira idade é maior que a primeira.
+
+idade1 = input('Digite a primeira idade: ')
+idade2 = input('Digite a segunda idade: ')
+
+maiorIdade = idade1 > idade2
+maiorIdade
