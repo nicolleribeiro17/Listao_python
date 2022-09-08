d5ac79ee65aa608c1f59012118faccbeb8f8d991
nicolleribeiro17 <nicolle.rib.p@gmail.com>
2022-09-08 15:47:29 -0300

    docs: refazendo exercicio listao com classes

---
+++ ex16_class.py
@@ -0,0 +1,26 @@
+# 16). Programa ex16.py:
+# Estou tentando entender os juros do meu banco. Para isto, ele me informou esta fórmula:
+# valor_final = valor_emprestimo + (valor_emprestimo * taxa * tempo)
+# onde que:
+# ● valor_emprestimo: É o valor que pegarei emprestado.
+# ● taxa: É o valor da taxa por mês. Por exemplo: se for 4% ao mês, o valor será 0.04.
+# ● tempo: Quantidade de meses que irei pagar o empréstimo.
+# Crie um programa que colete cada um destes valores para calcular o valor final que estarei pagando ao banco.
+
+class Emprestimo:
+    def __init__(self, value, taxa, time):
+        self.value = value
+        self.taxa = taxa
+        self.time = time
+
+    def calcula_valor_final(self):
+        valor_final = (self.value + (self.value * self.taxa * self.time))
+        return f'Valor final pago ao banco: {valor_final}'
+
+
+valor_emprestimo = float(input('Digite o valor de empréstimo requerido: '))
+taxa_banco = float(input('Digite a taxa mensal do banco: '))
+tempo = int(input('Digite o tempo de parcelamento em meses: '))
+
+valor_final = Emprestimo(valor_emprestimo, taxa_banco, tempo)
+print(valor_final.calcula_valor_final())
